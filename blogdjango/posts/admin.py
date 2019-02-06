eaf737e61a98cb5fe290cf254b172dfd6a883d1c
Priscila Moreira <priscila.moreira@superbuy.com.br>
2019-02-06 17:37:46 -0200

    Modifica??o do Model Post, para receber categoria

--- blogdjango/posts/admin.py
+++ blogdjango/posts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display= ['title', 'slug', 'created_at']
+    search_fields = ['title','slug'] #Exibe uma barra de pesquisa, onde é possível pesquisar por esses campos
+    prepopulated_fields ={'slug':('title',)}
+
+admin.site.register(Post, PostAdmin)
+
