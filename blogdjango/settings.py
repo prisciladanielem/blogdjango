bed98f5d76d850b1de4066004991242910f3bc33
Priscila Moreira <priscila.moreira@superbuy.com.br>
2019-02-06 17:39:30 -0200

    Criei a v?ria para receber imagem

--- blogdjango/settings.py
+++ blogdjango/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blogdjango.core'
+    'blogdjango.posts'
 ]
 
 MIDDLEWARE = [
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
@@ -119,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR,'blogdjango','media') #Variavel de imagem
+MEDIA_URL = '/media/'
