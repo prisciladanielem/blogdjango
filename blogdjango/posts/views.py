9e90db78dc6b51b14f026ca7045f0c0d87428a1c
Priscila Moreira <priscila.moreira@superbuy.com.br>
2019-02-06 17:39:06 -0200

    Criei a view de details e contact

--- blogdjango/posts/views.py
+++ blogdjango/posts/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from .models import Post
 
-# Create your views here.
+def home(request):
+    posts = Post.objects.all()
+    template_name = 'posts/home.html'
+    context = {
+        'posts':posts
+    }
+    return render(request,template_name,context)
+
+def details(request,slug):
+    post = get_object_or_404(Post,slug=slug)
+    context = {
+        'post':post
+    }
+    template_name = 'posts/details.html'
+    return render(request,template_name, context)
+
+def contact(request):
+    template_name = 'contact.html'
+    return render(request, template_name)
