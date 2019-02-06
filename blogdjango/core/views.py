816416e89007298652083fef43e077e6b401c1ad
Priscila Moreira <priscila.moreira@superbuy.com.br>
2019-02-06 11:26:02 -0200

    Criei as fun??es home e contact

--- blogdjango/core/views.py
+++ blogdjango/core/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    template_name = 'home.html'
+    return render(request,template_name)
+
+def contact(request):
+    template_name = 'contact.html'
+    return render(request, template_name)
