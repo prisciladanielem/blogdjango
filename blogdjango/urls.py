c61691436cd36a458d9d9076ce1d1e3249122813
Priscila Moreira <priscila.moreira@superbuy.com.br>
2019-02-06 11:27:19 -0200

    Mapeamento das urls das p?ginas home e contato

--- blogdjango/urls.py
+++ blogdjango/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from blogdjango.core.views import home,contact
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name="home"),
+    path('contato/', contact, name="contact"),
 ]
