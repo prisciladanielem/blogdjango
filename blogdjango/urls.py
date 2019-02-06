87b45d42b9751a1a539674d7d47fe271164e2d49
Priscila Moreira <priscila.moreira@superbuy.com.br>
2019-02-06 17:40:09 -0200

    Criei a url para os detalhes utilizando o slug

--- blogdjango/urls.py
+++ blogdjango/urls.py
@@ -15,10 +15,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from blogdjango.core.views import home,contact
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+from blogdjango.posts.views import home, details, contact
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name="home"),
+    path('post', home, name="home"),
     path('contato/', contact, name="contact"),
+    path('post/<str:slug>/', details, name="details"),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
