f75fc6eaee1cf04613f09288725c4e46fc17d71a
Priscila Moreira <priscila.moreira@superbuy.com.br>
2019-02-14 12:32:03 -0200

    Alterei as aplica??es para dentro da pasta apps

--- blogdjango/settings.py
+++ blogdjango/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blogdjango.posts'
+    'blogdjango.apps.core',
+    'blogdjango.apps.posts'
 ]
 
 MIDDLEWARE = [
