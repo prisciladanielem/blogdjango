9131e2703d4792ca5af16da099fc920f5b49c8d0
Priscila Moreira <priscila.moreira@superbuy.com.br>
2019-02-14 12:31:18 -0200

    Organizei as urls por aplica??o

--- blogdjango/urls.py
+++ blogdjango/urls.py
@@ -1,34 +1,14 @@
-"""blogdjango URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from django.conf import settings
 from django.conf.urls.static import static
 
-from blogdjango.posts.views import home, details, contact
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name="home"),
-    path('post', home, name="home"),
-    path('contato/', contact, name="contact"),
-    path('post/<str:slug>/', details, name="details"),
+    path('', include('blogdjango.apps.core.urls')),
+    path('posts/', include('blogdjango.apps.posts.urls')),
 ]
-
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
