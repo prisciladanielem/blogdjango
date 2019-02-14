196490890f98639cd9c0869a2f648df00600b8c8
Priscila Moreira <priscila.moreira@superbuy.com.br>
2019-02-14 12:30:10 -0200

    Exlus?o de diret?rios

--- blogdjango/posts/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-class Contact(forms.Form):
-    name = forms.CharField(label='Nome', max_length=100)
-    last_name = forms.CharField(label='Sobrenome', max_length=100)
-    email = forms.EmailField(label='Email')
-    subject = forms.CharField(label='Assunto', max_length=80)
-    message = forms.CharField(label='Mensagem', widget=forms.Textarea)
