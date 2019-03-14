b70307455a7fa805cb1cdfdc553f95e6fb683402
Priscila Moreira <priscila.moreira@superbuy.com.br>
2019-03-14 09:32:57 -0300

    Criei a op??o de adicionar post dentro do painel do user

--- blogdjango/apps/posts/urls.py
+++ blogdjango/apps/posts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from blogdjango.apps.posts.views import details
+from blogdjango.apps.posts.views import details, new_post, show_posts
 
 urlpatterns = [
+    path('novo-post/', new_post, name="new_post"),
+    path('meus-posts/', show_posts, name="show_posts"),
     path('<str:slug>/', details, name="details"),
 ]
--- blogdjango/apps/posts/views.py
+++ blogdjango/apps/posts/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
+
 from blogdjango.apps.posts.models import Post
+from blogdjango.apps.accounts.forms import PostForm
+
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import get_user_model
+from django.contrib import messages
+
+User = get_user_model()
 
 def details(request,slug):
     post = get_object_or_404(Post,slug=slug)
@@ -10,5 +18,30 @@ def details(request,slug):
         'posts':posts
     }
     template_name = 'posts/details.html'
-    return render(request,template_name, context)
+    return render(request, template_name, context)
 
+
+def new_post(request):
+    # user,a = Post.objects.get_or_create(user=request.user)
+    context = {}
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user
+            post.save()
+            messages.success(request, 'Post criado com sucesso!')
+            return redirect('dashboard')
+    else:
+        form = PostForm(instance=request.user)
+    template_name = 'posts/new_post.html'
+    context['form'] = form
+    return render (request, template_name, context)
+
+def show_posts(request):
+    posts = Post.objects.filter(user=request.user)
+    context = {
+        'posts': posts
+    }
+    template_name = 'posts/show_posts.html'
+    return render(request, template_name, context)
