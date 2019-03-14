ca461672ba692d3a2d0a6320a5d782058e23703f
Priscila Moreira <priscila.moreira@superbuy.com.br>
2019-03-14 09:32:11 -0300

    Adicionei a op??o de editar informa??es

--- blogdjango/apps/accounts/urls.py
+++ blogdjango/apps/accounts/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView
-from blogdjango.apps.accounts.views import register
+from django.contrib.auth.views import LoginView, LogoutView
+from blogdjango.apps.accounts.views import register,dashboard, edit
 
 urlpatterns = [
+    path('', dashboard, name="dashboard"),
     path('login/', LoginView.as_view(template_name='accounts/login.html') , name='login' ),
+    path('sair/', LogoutView.as_view(next_page='/') , name='logout' ),
+    path('editar-dados/', edit, name='edit' ),
     path('cadastre-se/', register, name="register"),
 ]
--- blogdjango/apps/accounts/views.py
+++ blogdjango/apps/accounts/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render, redirect
-from blogdjango.apps.accounts.forms import RegisterForm
-from django.contrib.auth import authenticate, login
+from blogdjango.apps.accounts.forms import RegisterForm, EditAccountForm
+from django.contrib.auth import authenticate, login, get_user_model
+from django.contrib import messages
+
+from django.contrib.auth.decorators import login_required
+
+User = get_user_model()
 
 def register(request):
-    template_name = 'accounts/register.html'
     if request.method == 'POST':
-        form = RegisterForm(request.POST)
+        form = RegisterForm(request.POST, request.FILES)
         if form.is_valid():
             user = form.save() #salva os dados no banco de dados
             user = authenticate(username = user.username, password = form.cleaned_data['password1'])
@@ -16,4 +20,26 @@ def register(request):
     context = {
     'form':form
     }
-    return render(request, template_name,context)
+    template_name = 'accounts/register.html'
+    return render(request, template_name, context)
+
+@login_required
+def dashboard(request):
+    template_name = 'accounts/dashboard.html'
+    context = {}
+    return render(request, template_name, context)
+
+@login_required
+def edit(request):
+    template_name = 'accounts/edit.html'
+    context = {}
+    if request.method == 'POST':
+        form = EditAccountForm(request.POST, request.FILES, instance=request.user) #O request.FILES faz o upload das imagens
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Dados alterados com sucesso!')
+            return redirect('dashboard')
+    else:
+        form = EditAccountForm(instance=request.user) # se não for post, formulaŕio vazio
+    context['form'] = form
+    return render(request, template_name, context) 
