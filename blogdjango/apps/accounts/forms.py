9e6233aacf45115177817100009ed7d100d05b87
Priscila Moreira <priscila.moreira@superbuy.com.br>
2019-03-14 09:32:11 -0300

    Adicionei a op??o de editar informa??es

--- blogdjango/apps/accounts/forms.py
+++ blogdjango/apps/accounts/forms.py
@@ -1,25 +1,50 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
-class RegisterForm(UserCreationForm):
-    first_name = forms.CharField(label='Primeiro nome', max_length=100)
-    last_name = forms.CharField(label='Último nome',max_length=100)
-    email = forms.EmailField(label='E-mail')
+from blogdjango.apps.posts.models import Post
 
-    #Função para o email ser único no cadastro
-    def clean_email(self):
-        email = self.cleaned_data['email']
-        if User.objects.filter(email=email).exists():
-            raise forms.ValidationError('E-mail já cadastrado!')
-        return email
+User = get_user_model()
 
-    #Função para salvar os dados no banco de dados
+class RegisterForm(forms.ModelForm): #Herda de UserCreationForm
+    password1 = forms.CharField(label='Senha', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Confirmação de Senha', widget=forms.PasswordInput)
+
+    #Verifica se as digitadas são iguais
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError('A confirmação da senha está incorreta')
+        return password2
+
+    #Função para salvar o email no banco de dados
     def save(self, commit=True):
         user = super(RegisterForm, self).save(commit=False)
-        user.email = self.cleaned_data['email']
-        user.first_name = self.cleaned_data['first_name']
-        user.last_name = self.cleaned_data['last_name']
+        user.set_password(self.cleaned_data['password1']) #set_password, criptografa a senha
         if commit:
             user.save()
         return user
+
+    class Meta:
+        model = User
+        fields = ['image','name','username', 'email']
+
+class EditAccountForm(forms.ModelForm): #ModelForm pega os campos do form
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        queryset =  User.objects.filter(email=email).exclude(pk=self.instance.pk)
+        if queryset.exists():
+            raise forms.ValidationError('E-mail já cadastrado!')
+        return email
+
+    class Meta:
+        model = User #Form que o modelForm vai pegar  os compos
+        fields = ['name','username', 'email','image'] #Campos que serão importados
+
+class PostForm(forms.ModelForm):
+
+    class Meta:
+        model = Post
+        fields = ['title','article','image','category',]
--- blogdjango/apps/accounts/models.py
+++ blogdjango/apps/accounts/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, UserManager
+from django.core.validators import RegexValidator
 
-# Create your models here.
+class User(AbstractBaseUser,PermissionsMixin):
+    username = models.CharField('Usuário',
+                                max_length=30,
+                                unique=True,
+                                validators=[RegexValidator(r'^[\w.@+-]+$', #Valida se existe catacter inválido no nome de usuário
+                                message='O nome de usuário pode ser somente letras, números e os caracteres @/ ./ -/ _/',
+                                code='invalid')])
+    email = models.EmailField('E-mail', unique=True)
+    name = models.CharField('Nome', max_length=100, blank=True)
+    image = models.ImageField(upload_to='accounts/images', verbose_name='Imagem', null=True, blank=True)
+    is_active = models.BooleanField('Está ativo?', blank=True, default=True)
+    is_staff = models.BooleanField('É da equipe?', blank=True, default=False)
+    data_joined = models.DateTimeField('Data de entrada',auto_now_add=True)
+ 
+    objects = UserManager()
+ 
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+ 
+    def __str__(self):
+        return self.name or self.username
+ 
+    def get_short_name(self):
+        return self.username
+ 
+    def get_full_name(self):
+        return str(self)
+ 
+    class Meta:
+        verbose_name = 'Usuário'
+        verbose_name_plural = 'Usuários'
