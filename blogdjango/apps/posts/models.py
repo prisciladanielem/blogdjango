7eab1216dfa726d1c4f537cb52b38e2bff432450
Priscila Moreira <priscila.moreira@superbuy.com.br>
2019-03-14 09:32:57 -0300

    Criei a op??o de adicionar post dentro do painel do user

--- blogdjango/apps/posts/models.py
+++ blogdjango/apps/posts/models.py
@@ -1,22 +1,26 @@
 from django.db import models
+from django.conf import settings
+from django.utils.text import slugify
 
 class PostManager(models.Manager):
     def search(self,query):
         return self.get_queryset().filter(models.Q(title__icontains=query) | models.Q(description__icontains=query))
 
 class Post(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, 
+                             verbose_name='Usuário',
+                             on_delete=models.CASCADE
+                            )
     title = models.CharField('Título', max_length=200)
     slug = models.SlugField('Atalho')
     article = models.TextField('Texto')
     categories = (
         ('sem categoria','sem categoria'),
-        ('web design','web wesign'),
-        ('photoshop','photoshop'),
-        ('html5 e css3','html5 e css3'),
-        ('web application','web application'),
-        ('seo','seo')
+        ('poema','poema'),
+        ('poesia','poesia'),
+        ('haikai','haikai')
     )
-    category = models.CharField(max_length=20, choices=categories, default='sem categoria')
+    category = models.CharField('Categoria',max_length=20, choices=categories, default='sem categoria')
     image = models.ImageField(upload_to='posts/images', verbose_name='Imagem', null=True, blank=True)
     created_at = models.DateTimeField('Criado em', auto_now_add=True)
     updated_at = models.DateTimeField('Atualizado em', auto_now=True)
@@ -26,3 +30,11 @@ class Post(models.Model):
     @models.permalink
     def get_absolute_url(self):
         return('details',(),({'slug':self.slug}))
+
+    class Meta:
+        ordering = ['-created_at']
+
+    #Preenche o slug automático quando o post é adicionado por dentro do sistema
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title, allow_unicode=True)
+        return super(Post, self).save(*args, **kwargs)
