8708cb74f1516a4ef4cb30f1debc4a717e27384a
Priscila Moreira <priscila.moreira@superbuy.com.br>
2019-03-15 16:04:38 -0300

    Criei as urls para editar, mostrar e excluir posts

--- blogdjango/apps/posts/urls.py
+++ blogdjango/apps/posts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from blogdjango.apps.posts.views import details, new_post, show_posts
+from blogdjango.apps.posts.views import details, new_post, edit_post, delete_post, show_posts
 
 urlpatterns = [
     path('novo-post/', new_post, name="new_post"),
+    path('excluir-post/<str:slug>/', delete_post, name="delete_post"),
+    path('editar-post/<str:slug>/', edit_post, name="edit_post"),
     path('meus-posts/', show_posts, name="show_posts"),
     path('<str:slug>/', details, name="details"),
 ]
