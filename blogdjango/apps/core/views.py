0c41aa5ede1ef0d267f3d01afa47feda5e19df21
Priscila Moreira <priscila.moreira@superbuy.com.br>
2019-03-15 15:58:41 -0300

    Adicionei a pagina na p?gina inicial

--- blogdjango/apps/core/views.py
+++ blogdjango/apps/core/views.py
@@ -1,21 +1,30 @@
 from django.shortcuts import render
-from blogdjango.apps.posts.models import Post
+from django.core.paginator import Paginator
 
+from blogdjango.apps.posts.models import Post
 from blogdjango.apps.posts.forms import Contact
 
 def base(request):
-    posts = Post.objects.all()
+    posts_recents = Post.objects.all()
     template_name = 'base.html'
     context = {
-        'posts':posts
+        'posts_recents':posts_recents
     }
     return render(request,template_name,context)
 
 def home(request):
-    posts = Post.objects.all()
+    posts_list = Post.objects.all()
+    page = request.GET.get('page', 1)
+    paginator = Paginator(posts_list, 4)
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
     template_name = 'posts/home.html'
     context = {
-        'posts':posts
+        'posts':posts,
     }
     return render(request,template_name,context)
 
@@ -25,6 +34,6 @@ def contact(request):
     form = Contact()
     context = {
         'form' : form,
-        'posts':posts 
+        'posts' : posts 
                 }
     return render(request, template_name, context)
