48fa5125f41ff32aff6eefc291f5a285adef377a
Priscila Moreira <priscila.moreira@superbuy.com.br>
2019-03-15 16:05:17 -0300

    Criei as views para editar, mostrar e excluir posts

--- blogdjango/apps/posts/views.py
+++ blogdjango/apps/posts/views.py
@@ -3,10 +3,13 @@ from django.http import HttpResponse
 
 from blogdjango.apps.posts.models import Post
 from blogdjango.apps.accounts.forms import PostForm
+from .forms import EditPostForm
 
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import get_user_model
 from django.contrib import messages
+
+from django.core.paginator import Paginator
 
 User = get_user_model()
 
@@ -20,7 +23,7 @@ def details(request,slug):
     template_name = 'posts/details.html'
     return render(request, template_name, context)
 
-
+@login_required
 def new_post(request):
     # user,a = Post.objects.get_or_create(user=request.user)
     context = {}
@@ -33,13 +36,51 @@ def new_post(request):
             messages.success(request, 'Post criado com sucesso!')
             return redirect('dashboard')
     else:
-        form = PostForm(instance=request.user)
+        form = PostForm()
     template_name = 'posts/new_post.html'
     context['form'] = form
     return render (request, template_name, context)
 
+@login_required
+def edit_post(request,slug):
+    post = get_object_or_404(Post,slug=slug)
+    context = {}
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES, instance=post) #O request.FILES faz o upload das imagens
+        if form.is_valid():
+            form.save()
+            messages.success(request,'Post alterado com sucesso!')
+            return redirect('dashboard')
+    else:
+        form = PostForm(instance=post) # se não for post, formulaŕio vazio
+    context['form'] = form
+    template_name = 'posts/edit_post.html'
+    return render(request, template_name, context)
+
+@login_required
+def delete_post(request,slug):
+    post = get_object_or_404(Post,slug=slug,user = request.user) 
+    if request.method=='POST': # Testa se o usuário quer cancelar a inscrição
+        post.delete() # deleta a inscrição
+        messages.success(request,'Post excluído com sucesso!')
+        return redirect('dashboard')
+    context = {
+        'post': post,
+    }
+    template_name = 'posts/delete_post.html'
+    return render(request, template_name, context)
+
+@login_required
 def show_posts(request):
-    posts = Post.objects.filter(user=request.user)
+    posts_list = Post.objects.filter(user=request.user)
+    page = request.GET.get('page', 1)
+    paginator = Paginator(posts_list, 4)
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
     context = {
         'posts': posts
     }
