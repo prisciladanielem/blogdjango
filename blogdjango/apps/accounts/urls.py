b87ceeb6914b7dee813cc1292336fac2a78e230f
Priscila Moreira <priscila.moreira@superbuy.com.br>
2019-03-15 16:39:51 -0300

    Implementa??o da altera??o de senha

--- blogdjango/apps/accounts/urls.py
+++ blogdjango/apps/accounts/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-from blogdjango.apps.accounts.views import register,dashboard, edit
+from blogdjango.apps.accounts.views import register,dashboard, edit, edit_password
 
 urlpatterns = [
     path('', dashboard, name="dashboard"),
     path('login/', LoginView.as_view(template_name='accounts/login.html') , name='login' ),
     path('sair/', LogoutView.as_view(next_page='/') , name='logout' ),
     path('editar-dados/', edit, name='edit' ),
+    path('alterar-senha/', edit_password, name='edit_password'),
     path('cadastre-se/', register, name="register"),
 ]
